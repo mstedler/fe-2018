8e1ad7958ecf4ab7bdf747f2652ce8ed37492dad
Mateus Stedler <mstedlerr@gmail.com>
2018-09-04 18:48:19 -0300

    refatorar estrutura
    padronizacao dos ids para conformidade com jquery

---
+++ js/main.js
@@ -0,0 +1,30 @@
+function scaleOutTimerIn(element, time) {
+    $(element).toggleClass("scale-out");
+    setTimeout(function () {
+        $(element).toggleClass("scale-out");
+    }, time);
+}
+$(document).ready(function () {
+    $('.dropdown-trigger').dropdown();
+    $('.tabs').tabs();
+    $('select').formSelect();
+    $('.datepicker').datepicker({ format: "dd/mm/yyyy", autoClose: true, disableWeekends: true });
+
+    $("#tipo-identificador")
+        .change(function () {
+            $("#tipo-identificador option:selected").each(function () {
+                $("#ctps, #titulo-eleitor, #certidao").hide();
+                switch ($(this).val()) {
+                    case "18":
+                        $("#ctps").show();
+                        break;
+                    case "19":
+                        $("#titulo-eleitor").show();
+                        break;
+                    case "20":
+                        $("#certidao").show();
+                        break;
+                }
+            });
+        });
+});
